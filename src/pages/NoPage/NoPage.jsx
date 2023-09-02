1988511aec9a7654bb90bd2143959e4f4965b43f
TemurEShtemirov <temureshtemirov10@gmail.com>
2023-09-03 00:54:04 +0500

    Fixed

--- src/pages/NoPage/NoPage.jsx
+++ src/pages/NoPage/NoPage.jsx
@@ -3,7 +3,7 @@ export default function NoPage() {
   return (
     <>
     <div className="NoPage">
-        <h1>404</h1>
+        <h1 className="fs-1 text-center text-danger">404 PAGE IS NOT FOUND !!!!!!!!</h1>
     </div>
     </>
   )
--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,24 +1,25 @@
-
+import { Link,Outlet } from "react-router-dom";
+import React from "react";
 export default function Layout() {
     return (
         <>
             <div className="Layout">
                 <ul className="nav justify-content-end">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Active</a>
+                        <Link  to="/products" className="nav-link active" aria-current="page" >Products</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
+                        <Link to="/" className="nav-link">Home</Link>
                     </li>
-                    <li className="nav-item">
+                    {/* <li className="nav-item">
                         <a className="nav-link" href="#">Link</a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
+                    </li> */}
                 </ul>
-
             </div>
+<Outlet/>
         </>
     )
 }
